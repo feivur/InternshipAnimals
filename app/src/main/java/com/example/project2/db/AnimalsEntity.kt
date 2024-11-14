dc0bd9fcb5a121b1a9463c181719dcd6334dc71b
Ivan Fedeikin <ivan.fedeikin@axxonsoft.dev>
2024-11-14 14:19:16 +0300

    update: AnimalEntity

--- app/src/main/java/com/example/project2/db/AnimalsEntity.kt
+++ app/src/main/java/com/example/project2/db/AnimalsEntity.kt
@@ -3,13 +3,15 @@ package com.example.project2.db
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.project2.AnimalType
 
 @Entity(tableName = "animals")
 data class AnimalsEntity(
 
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    @ColumnInfo(name = "animal_type") val type: String?,
+    @ColumnInfo(name = "animal_type") val type: AnimalType,
 //    @ColumnInfo(name = "animal") val animal: Animal,
     @ColumnInfo(name = "name") val name: String?,
     @ColumnInfo(name = "color") val color: String?,
+    @ColumnInfo(name = "color") val width: Int
 )
